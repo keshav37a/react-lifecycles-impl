7f68174930810d125409e2d788096df4ab6cc4f4
Keshav Mathur <keshav37a@gmail.com>
2020-09-02 20:45:12 +0530

    addlifecycle hooks

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,17 +1,95 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
-class Counter extends Component{
-    constructor(props){
-        super(props);
-        console.log('[Counter.js] constructor called');
-    }
+const ErrorComponent = () => {
+  return <div>{this.props.nonExistantValueToTriggerError}</div>;
+};
 
-    render(){
-        console.log('[Counter.js] render called');
-        return(
-            <div>Counter</div>
-        )
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[Counter.js] constructor called");
+    this.state = {
+      counter: 0,
+      seed: 0,
+      showError: false
+    };
+  }
+
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  decrement = () => {
+    let currentCount = this.state.counter;
+    if (currentCount > 0) this.setState({ counter: this.state.counter - 1 });
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+        console.log(props);
+      return {
+        seed: props.seed,
+        counter: props.seed,
+        showError: props.showError
+      };
     }
+    return null;
+  }
+
+  componentDidCatch(error, info) {
+    console.log("[Counter.js] componentDidCatch");
+    console.log(error);
+    this.setState({ error, info });
+  }
+
+  componentDidMount() {
+    console.log("[Counter.js] componentDidMount");
+    console.log("---------------------------------------");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps);
+    // if(nextProps.ignoreProp && this.props.isProp !== nextProps.ignoreProp ){
+    //     console.log('[Counter.js] shouldComponentUpdate - NOT RENDER');
+    //     return false;
+    // }
+    // console.log('[Counter.js] shouldComponentUpdate - RENDER');
+    return true;
+  }
+
+  // Capture some properties that are not stored in the state before we rerender that component
+  getSnapshotBeforeUpdate() {
+
+  }
+
+  render() {
+    console.log("[Counter.js] render called");
+
+    if (this.state.error) {
+      console.log(this.state.error);
+      return <div>We have encountered an error {this.state.error.message}</div>;
+    } else {
+      return (
+        <React.Fragment>
+          <div>Counter: {this.state.counter}</div>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+          {this.state.showError ? <ErrorComponent /> : null}
+        </React.Fragment>
+      );
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Counter.js] componentDidUpdate ", this.state.counter);
+    console.log(prevState);
+    console.log(snapshot);
+    console.log("-------------------------------------------------");
+  }
+
+  componentWillUnmount() {
+    console.log("[Counter.js] componentWillUnmount");
+  }
 }
 
 export default Counter;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Counter from "../components/counter/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: true,
+      ignoreProp: 0,
+      seed: 40,
+      showError: false
+    };
+  }
+
+  mountCounter = () => {
+    this.setState({ mount: true });
+  };
+
+  unMountCounter = () => {
+    this.setState({ mount: false });
+  };
+
+  ignoreProp = () => {
+    this.setState({ignoreProp: Math.random()})
+  }
+
+  seedGenerator = () =>{
+    this.setState({seed: Number.parseInt(Math.random()*100)});
+  }
+
+  toggleError = () =>{
+    console.log(this.state.showError);
+    this.setState({showError: !this.state.showError});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</button>
+        <button onClick={this.unMountCounter} disabled={!this.state.mount}>Unmount Counter</button>
+        <button onClick={this.ignoreProp}>ignoreProp</button>
+        <button onClick={this.seedGenerator}>Generate Seed</button>
+        <button onClick={this.toggleError}>Toggle Error</button>
+        {this.state.mount === true ? 
+          <Counter 
+            ignoreProp={this.state.ignoreProp} 
+            seed={this.state.seed}
+            showError={this.state.showError}/> 
+          : null}
+      </div>
+    );
+  }
 }
 
 export default App;
